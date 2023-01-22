13ca315d74ff7adfa834c46e7e4641bf1ec31a40
Vlasov Artem [WS] <root@proscript.ru>
2023-01-22 21:39:40 +0400

    Add catdoc extract method for DOC files

--- textract/parsers/doc_parser.py
+++ textract/parsers/doc_parser.py
@@ -1,3 +1,5 @@
+from ..exceptions import UnknownMethod
+
 from .utils import ShellParser
 
 
@@ -5,6 +7,20 @@ class Parser(ShellParser):
     """Extract text from doc files using antiword.
     """
 
-    def extract(self, filename, **kwargs):
+    def extract(self, filename, method='', **kwargs):
+        if not method or method == 'antiword':
+            return self.extract_antiword(filename=filename, **kwargs)
+
+        elif method == 'catdoc':
+            return self.extract_catdoc(filename, **kwargs)
+
+        else:
+            raise UnknownMethod(method)
+
+    def extract_antiword(self, filename, **kwargs):
         stdout, stderr = self.run(['antiword', filename])
+        return stdout
+
+    def extract_catdoc(self, filename, **kwargs):
+        stdout, stderr = self.run(['catdoc', filename])
         return stdout
